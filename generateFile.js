26cd5573295347bfffd60f279ce322a78e42dcd3
MayankGEHU <minku6195@gmail.com>
2025-05-18 00:11:10 +0530

    backend

--- generateFile.js
+++ generateFile.js
@@ -1,19 +1,28 @@
-const fs = require("fs");
+const fs = require("fs/promises");
 const path = require("path");
 const { v4: uuid } = require("uuid");
 
 const dirCodes = path.join(__dirname, "codes");
 
-if (!fs.existsSync(dirCodes)) {
-  fs.mkdirSync(dirCodes, { recursive: true });
-}
+(async () => {
+  try {
+    await fs.mkdir(dirCodes, { recursive: true });
+  } catch (err) {
+    console.error("Error creating codes directory:", err);
+  }
+})();
 
 const generateFile = async (format, content) => {
   const jobId = uuid();
   const filename = `${jobId}.${format}`;
   const filepath = path.join(dirCodes, filename);
-  await fs.writeFileSync(filepath, content);
-  return filepath;
+  try {
+    await fs.writeFile(filepath, content);
+    return filepath;
+  } catch (err) {
+    console.error("Error writing file:", err);
+    throw err;
+  }
 };
 
 module.exports = {
